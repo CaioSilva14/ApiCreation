eaf675598cfe6cd721a20ccb1cf182c159c2cf93
Caio Silva <csm14103@gmail.com>
2024-08-20 23:40:51 -0300

    Implementando algoritmo da api

---
+++ server.js
@@ -0,0 +1,32 @@
+import express from 'express'
+import { PrismaClient } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const app = express()
+app.use(express.json())
+const users = []
+
+app.post('/users', async (req, res) => {
+    
+    await prisma.user.create({
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+    res.status(201).json(req.body)
+
+})
+
+app.get('/users', (req, res) => {
+    
+    res.status(200).json(users)
+
+
+})
+
+app.listen(3000)
+
+// caio cEWxRkm2jo8RHR0V cEWxRkm2jo8RHR0V npx prisma studio node --watch server.js
