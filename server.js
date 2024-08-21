a1af4d4c9bf238b1f0f474d2e73d824d1f4d6fa1
Caio Silva <csm14103@gmail.com>
2024-08-21 00:37:27 -0300

    Implementação métodos put e delete

--- server.js
+++ server.js
@@ -5,7 +5,6 @@ const prisma = new PrismaClient()
 
 const app = express()
 app.use(express.json())
-const users = []
 
 app.post('/users', async (req, res) => {
     
@@ -20,13 +19,41 @@ app.post('/users', async (req, res) => {
 
 })
 
-app.get('/users', (req, res) => {
+app.get('/users', async (req, res) => {
     
+    const users = await prisma.user.findMany()
+
     res.status(200).json(users)
 
+})
 
+app.put('/users/:id', async (req, res) => {
+    
+    await prisma.user.update({
+        where: {
+            id: req.params.id
+        },
+        data: {
+            email: req.body.email,
+            name: req.body.name,
+            age: req.body.age
+        }
+    })
+    res.status(201).json(req.body)
+
+})
+
+app.delete('/users/:id', async (req, res) => {
+    
+    await prisma.user.delete({
+        where: {
+            id: req.params.id
+        }
+    })
+
+    res.status(200).json({message: 'Usuário deletado com sucesso.'})
 })
 
 app.listen(3000)
 
-// caio cEWxRkm2jo8RHR0V cEWxRkm2jo8RHR0V npx prisma studio node --watch server.js
+// caio cEWxRkm2jo8RHR0V npx prisma studio node --watch server.js
